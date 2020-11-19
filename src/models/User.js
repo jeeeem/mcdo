7280c97ac7880c3cf9e58a0393e5187aa96d3bc3
Jim Angelo Beren <jim.beren07@gmail.com>
2020-11-19 18:02:59 +0800

    init

--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,33 @@
-const Sequelize = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const connection = require('../database/db');
 
-//const User = connection.define('user', {
-//id: {
-//type: Sequelize.INTEGER,
-//autoIncrement: true,
-//primaryKey: true,
-//},
-//username: {
-//type: Sequelize.STRING(25),
-//allowNull: false,
-//unique: true,
-//},
-//password_hash: {
-//type: Sequelize.STRING(20),
-//allowNull: false,
-//},
-//});
-const { QueryTypes } = require('sequelize');
-const users = connection
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+  },
+  {
+    connection,
+    modelName: User,
+  }
+);
+
+console.log(User === connection.models.User);
+/*const users = connection
   .query('SELECT * FROM `users`', {
     type: QueryTypes.SELECT,
   })
@@ -27,12 +36,23 @@ const users = connection
   })
   .catch((err) => {
     console.log(err);
-  });
+  });*/
+
+/*const users = connection
+  .query('INSERT INTO `users` values (4, "test4",  123)', {
+    type: QueryTypes.INSERT,
+  })
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });*/
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const myPlaintextPassword = 's0//P4$$w0rD';
-const someOtherPljjintextPassword = 'not_bacon';
+const someOtherPlaintextPassword = 'not_bacon';
 
 bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
   // Store hash in your password DB.
